4c888df334972ce586767c5addd3f527b8db7ed8
rishabhkhade <rishabhkhade95@gmail.com>
2025-03-31 12:01:35 +0530

    changes

--- src/componant/dashboard/Dashboard.js
+++ src/componant/dashboard/Dashboard.js
@@ -1,127 +1,136 @@
 import React from "react";
 import "./dashboard.scss";
-import { Space, Table } from 'antd';
+import { Space, Table } from "antd";
 import { CiEdit } from "react-icons/ci";
 import { MdDeleteForever } from "react-icons/md";
 
 const Dashboard = () => {
-
   //subdomain data
   const columns = [
     {
-      title: 'Sr No',
-      dataIndex: 'srNo',
-      key: 'srNo',
-      width: '5%',
+      title: "Sr No",
+      dataIndex: "srNo",
+      key: "srNo",
+      width: "5%",
     },
     {
-      title: 'Subdomain Name',
-      dataIndex: 'name',
-      key: 'name',
-      width: '20%',
+      title: "Subdomain Name",
+      dataIndex: "name",
+      key: "name",
+      width: "20%",
     },
     {
-      title: 'Description',
-      dataIndex: 'description',
-      key: 'description',
-      width: '70%',
+      title: "Description",
+      dataIndex: "description",
+      key: "description",
+      width: "70%",
     },
     {
-      title: 'Action',
-      key: 'action',
+      title: "Action",
+      key: "action",
       render: (_, record) => (
         <Space size="middle">
-          <a href="#"><CiEdit /></a>
-          <a href="#"><MdDeleteForever /></a>
+          <a href="#">
+            <CiEdit />
+          </a>
+          <a href="#">
+            <MdDeleteForever />
+          </a>
         </Space>
       ),
-      width: '5%',
+      width: "5%",
     },
   ];
 
   const data = [
     {
-      key: '1',
-      srNo: '1',
-      name: 'test.diwise.in',
-      description: 'I ve added a Sr No column and adjusted the column widths to distribute 100% evenly. Let me know if yod like further adjustments or styling enhancements!',
+      key: "1",
+      srNo: "1",
+      name: "test.diwise.in",
+      description:
+        "I ve added a Sr No column and adjusted the column widths to distribute 100% evenly. Let me know if yod like further adjustments or styling enhancements!",
     },
     {
-      key: '2',
-      srNo: '2',
-      name: 'Jim Green',
-      description: 'I ve added a Sr No column and adjusted the column widths to distribute 100% evenly. Let me know if yod like further adjustments or styling enhancements!',
+      key: "2",
+      srNo: "2",
+      name: "Jim Green",
+      description:
+        "I ve added a Sr No column and adjusted the column widths to distribute 100% evenly. Let me know if yod like further adjustments or styling enhancements!",
     },
     {
-      key: '3',
-      srNo: '3',
-      name: 'Joe Black',
-      description: 'I ve added a Sr No column and adjusted the column widths to distribute 100% evenly. Let me know if yod like further adjustments or styling enhancements!',
+      key: "3",
+      srNo: "3",
+      name: "Joe Black",
+      description:
+        "I ve added a Sr No column and adjusted the column widths to distribute 100% evenly. Let me know if yod like further adjustments or styling enhancements!",
     },
   ];
   //end subdomain data
 
-   //website data
-   const columnsweb = [
+  //website data
+  const columnsweb = [
     {
-      title: 'Sr No',
-      dataIndex: 'srNo',
-      key: 'srNo',
-      width: '5%',
+      title: "Sr No",
+      dataIndex: "srNo",
+      key: "srNo",
+      width: "5%",
     },
     {
-      title: 'Website URL',
-      dataIndex: 'url',
-      key: 'url',
-      width: '20%',
+      title: "Website URL",
+      dataIndex: "url",
+      key: "url",
+      width: "20%",
     },
     {
-      title: 'Username',
-      dataIndex: 'username',
-      key: 'username',
-      width: '35%',
+      title: "Username",
+      dataIndex: "username",
+      key: "username",
+      width: "35%",
     },
     {
-      title: 'Password',
-      dataIndex: 'password',
-      key: 'password',
-      width: '35%',
+      title: "Password",
+      dataIndex: "password",
+      key: "password",
+      width: "35%",
     },
     {
-      title: 'Action',
-      key: 'action',
+      title: "Action",
+      key: "action",
       render: (_, record) => (
         <Space size="middle">
-          <a href="#"><CiEdit /></a>
-          <a href="#"><MdDeleteForever /></a>
+          <a href="#">
+            <CiEdit />
+          </a>
+          <a href="#">
+            <MdDeleteForever />
+          </a>
         </Space>
       ),
-      width: '5%',
+      width: "5%",
     },
   ];
 
   const webdata = [
     {
-      key: '1',
-      srNo: '1',
-      url: 'https://test.diwise.in',
-      username:'new_user_diwise',
-      password:'new_user_diwise@2025',
+      key: "1",
+      srNo: "1",
+      url: "https://test.diwise.in",
+      username: "new_user_diwise",
+      password: "new_user_diwise@2025",
     },
     {
-      key: '2',
-      srNo: '2',
-      url: 'https://test.diwise.in',
-      username:'new_user_diwise',
-      password:'new_user_diwise@2025',
-      
+      key: "2",
+      srNo: "2",
+      url: "https://test.diwise.in",
+      username: "new_user_diwise",
+      password: "new_user_diwise@2025",
     },
     {
-      key: '3',
-      srNo: '3',
-      url: 'https://test.diwise.in',
-      username:'new_user_diwise',
-      password:'new_user_diwise@2025',
+      key: "3",
+      srNo: "3",
+      url: "https://test.diwise.in",
+      username: "new_user_diwise",
+      password: "new_user_diwise@2025",
     },
   ];
   //end website data
@@ -135,35 +144,37 @@ const Dashboard = () => {
             <h3>Domain Details:</h3>
             <div className="domain-details">
               <div className="detail-left">
-                <p>Domain Name : Lorem ipsum dolor sit. </p>
-                <p>Purchase Date: pqr </p>
-                <p>Expiry Date: pqr </p>
-                <p>Purchase Platform: pqr </p>
+                <p>Domain Name : <span>Lorem ipsum dolor sit.</span> </p>
+                <p>Purchase Platform : <span>15/3/24</span> </p>
+                <p>Purchase Date : <span>pqr</span> </p>
+                <p>Expiry Date : <span>12/2/25</span> </p>
               </div>
             </div>
+
+           
+            <div className="login-detail">
+              <p>
+                Login URL : <span>https://hotinger.com</span>
+              </p>
+              <p>
+                User Name : <span>this is username</span>
+              </p>
+              <p>
+                Password : <span>this is password</span>
+              </p>
+            </div>
           </div>
-{/* //----------------Details for all created subdomain -------------------// */}
+          {/* //----------------Details for all created subdomain -------------------// */}
           <div className="middle-section">
             <h3>Subdomain List:</h3>
             <div className="subdomain-list">
-              <Table 
-                columns={columns} 
-                dataSource={data} 
-                pagination={{ pageSize: 5 }} 
-                bordered 
+              <Table
+                columns={columns}
+                dataSource={data}
+                pagination={{ pageSize: 5 }}
+                bordered
               />
             </div>
-          </div>
-
-          {/* //-------------------Third Section For Login Details------------// */}
-          <div className="third-section">
-          <h3>Domain Login Details:</h3>
-          <div className="login-detail">
-          <p>Login URL: <span>https://hotinger.com</span></p>
-          <p>Username: <span>this is username</span></p>
-          <p>Password: <span>this is password</span></p>
-          </div>
-        
           </div>
 
           {/* //-------------------Website login details ----------------// */}
@@ -171,11 +182,11 @@ const Dashboard = () => {
           <div className="fourth-section">
             <h3>Website Login Details</h3>
             <div className="website-list">
-              <Table 
-                columns={columnsweb} 
-                dataSource={webdata} 
-                pagination={{ pageSize: 5 }} 
-                bordered 
+              <Table
+                columns={columnsweb}
+                dataSource={webdata}
+                pagination={{ pageSize: 5 }}
+                bordered
               />
             </div>
           </div>
