c546a97a760b4e43bcd561a2e59c407d51cc9c52
rishabhkhade <rishabhkhade95@gmail.com>
2025-04-01 15:10:48 +0530

    add website api

--- src/pages/add-website/AddWebsite.js
+++ src/pages/add-website/AddWebsite.js
@@ -1,36 +1,82 @@
 import React, { useState } from "react";
-import './addwebsite.scss'
+import "./addwebsite.scss";
+import axios from "axios";
+
 const AddWebsite = () => {
+  const [webSite, setWebSite] = useState({
+    websiteUrl: "",
+    websitePlatfrom: "",
+    username: "",
+    password: "",
+  });
 
-
-
-const [webSite, setWebSite] = useState({
-  frontendUrl:"",
-  backendUrl:"",
-  credentials:[]
-})
-
+  const handleWebsite = async () => {
+    try {
+      const response = await axios.post(
+        `${process.env.REACT_APP_API}api/website/addWebsite`,
+        webSite
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <>
       <div className="add-website-parent parent">
         <div className="add-website-cont cont">
-          <form class="add-domain-box">
+          <form class="add-domain-box" onSubmit={handleWebsite}>
             <label>
               <p>Website URL</p>
-              <input type="text" />
+              <input
+                type="text"
+                value={webSite.websiteUrl}
+                onChange={(e) =>
+                  setWebSite({
+                    ...webSite,
+                    websiteUrl: e.target.value,
+                  })
+                }
+              />
             </label>
             <label>
               <p>Website Platform</p>
-              <input type="text" />
+              <input
+                type="text"
+                value={webSite.websitePlatfrom}
+                onChange={(e) =>
+                  setWebSite({
+                    ...webSite,
+                    websitePlatfrom: e.target.value,
+                  })
+                }
+              />
             </label>
             <label>
               <p>Username</p>
-              <input type="text" />
+              <input
+                type="text"
+                value={webSite.username}
+                onChange={(e) =>
+                  setWebSite({
+                    ...webSite,
+                    username: e.target.value,
+                  })
+                }
+              />
             </label>
             <label>
               <p>Password</p>
-              <input type="text" />
+              <input
+                type="text"
+                value={webSite.password}
+                onChange={(e) =>
+                  setWebSite({
+                    ...webSite,
+                    password: e.target.value,
+                  })
+                }
+              />
             </label>
             <button class="btn" type="submit">
               Submit
