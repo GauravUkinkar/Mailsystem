7ef15ae754b67833d2d7dae97c73baf7b5820a2e
Sunil Shelke <shelkesunil072@gmail.com>
2025-04-30 12:48:41 +0530

    new asynding

--- src/componant/Sidebar.js
+++ src/componant/Sidebar.js
@@ -7,7 +7,6 @@ import {
   useNavigate,
   useSearchParams,
 } from "react-router-dom";
-import { FcDeleteColumn } from "react-icons/fc";
 import axios from "axios";
 const Sidebar = ({ onLogout, getDomains }) => {
   const [searchParams] = useSearchParams();
@@ -18,8 +17,7 @@ const Sidebar = ({ onLogout, getDomains }) => {
 
   const defaultId = getDomains[0]?.id;
 
-
-
+  console.log(getDomains[0]?.Domain, "klfskjldsjkdfskljdfskldfs");
 
   const idFromParams = searchParams.get("id");
 
@@ -35,60 +33,68 @@ const Sidebar = ({ onLogout, getDomains }) => {
 
   const setIdInUrl = (newId) => {
     const params = new URLSearchParams(window.location.search);
-    params.set("id", newId); // set new id value
-  
+    params.set("id", newId);
+
     const newUrl = `${window.location.pathname}?${params.toString()}`;
-    window.history.replaceState({}, '', newUrl); // update URL without reloading
+    window.history.replaceState({}, "", newUrl); 
   };
-  
 
   //Call Domain Delete API
   const handleDelete = async (id) => {
     try {
-      const confirmDelete = window.confirm("Are you sure you want to delete this domain?");
+      const confirmDelete = window.confirm(
+        "Are you sure you want to delete this domain?"
+      );
       if (!confirmDelete) return;
-  
+
       await axios.delete(
         `${process.env.REACT_APP_API}api/domain/deleteDomain?domainId=${id}`
       );
-  
+
       alert("Domain Deleted Successfully");
-    
+
       if (defaultId !== idFromParams) {
-        navigate("/")
+        navigate("/");
       }
 
-      window.location.reload(); 
+      window.location.reload();
     } catch (error) {
       console.error("Error deleting domain:", error);
     }
   };
-  
-  
 
   return (
     <>
       <div className="sidebar-parent">
         <ul className="list">
-          {getDomains.map((item) => (
-            <li
-              key={item.id}
-              onClick={() => handleClick(item.id)}
-              className={`${
-                Number(active) === item.id ? "active side_item" : "side_item"
-              }`}
-            >
-              {item.Domain}
-              <div className="btn-delete" onClick={(e) => e.stopPropagation()}>
-                <button className="delete-btn" onClick={() => handleDelete(item.id)}><MdDeleteForever /></button>
-              </div>
-            </li>
-          ))}
-
-
+          {getDomains
+            .slice()
+            .sort((a, b) => a.Domain.localeCompare(b.Domain))
+            .map((item) => (
+              <li
+                key={item.id}
+                onClick={() => handleClick(item.id)}
+                className={`${
+                  Number(active) === item.id ? "active side_item" : "side_item"
+                }`}
+              >
+                {item.Domain}
+                <div
+                  className="btn-delete"
+                  onClick={(e) => e.stopPropagation()}
+                >
+                  <button
+                    className="delete-btn"
+                    onClick={() => handleDelete(item.id)}
+                  >
+                    <MdDeleteForever />
+                  </button>
+                </div>
+              </li>
+            ))}
         </ul>
 
-        <div className="logout-btn" >
+        <div className="logout-btn">
           <Link to="/" className="btn" onClick={onLogout}>
             Log Out
             <div className="icon">
